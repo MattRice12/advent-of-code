a50935538cc3e402b06cf64dd53153deed1a204a
mattrice12 <mattrice12@outlook.com>
2020-12-07 11:51:22 -0600

    day 7

---
+++ day7/day7.rb
@@ -0,0 +1,53 @@
+rules = File.open("./data.txt").readlines
+
+MY_BAG = "shiny gold"
+
+total_shiny_gold_bags = 0
+nested_bags_count = 0
+
+rule_set = rules.map do |rule|
+  key, value = rule.split(" bags contain ")
+  value = value.split(", ")
+  value = value.map do |v| 
+    {
+      name: key,
+      count: v.split(" ")[0],
+      contains: v.split(" ")[1..2].join(" ")
+    }
+  end
+  [key, value]
+end.to_h
+
+def find_shiny_gold(rule_set, rule)
+  return true if rule.any? { |bag| bag[:contains] == MY_BAG }
+
+  rule.any? do |bag|
+    next if !rule_set[bag[:contains]]
+    value = rule_set[bag[:contains]]
+    find_shiny_gold(rule_set, value)
+  end
+end
+
+def count_nested_bags(rule_set, top_level_bag)
+  count = top_level_bag.map do |bag|
+    if bag[:count].to_i == 0 # if bag contains no other bags return 0
+      0
+    else
+      # (count nested bags
+        # per number of top_level_bags)
+      # plus the top_level_bags
+      (count_nested_bags(rule_set, rule_set[bag[:contains]]) *
+        bag[:count].to_i) +
+      bag[:count].to_i
+    end
+  end
+  count.inject(:+)
+end
+
+rule_set.each do |key, value|
+  total_shiny_gold_bags += 1 if find_shiny_gold(rule_set, value)
+  nested_bags_count = count_nested_bags(rule_set, rule_set[MY_BAG])
+end
+
+puts total_shiny_gold_bags
+puts nested_bags_count
