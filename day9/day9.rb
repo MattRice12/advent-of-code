b6301b299a81de4069e39a9ce2f94aa348e19210
mattrice12 <mattrice12@outlook.com>
2020-12-09 16:17:39 -0600

    day9

--- day9/day9.rb
+++ day9/day9.rb
@@ -7,7 +7,7 @@ index_start = 0
 index_end = index_start + PREAMBLE
 
 class VM
-  attr_accessor :rules, :target_value, :index_start, :index_end
+  attr_accessor :rules, :target_value, :index_start, :index_end, :running, :encryption_weakness
   
   PREAMBLE = 25
 
@@ -17,23 +17,48 @@ class VM
     @index_start = 0
     @index_end = index_start + PREAMBLE
     @target_value = rules[index_end + 1]
+    @encryption_weakness = 0
     @running = true
   end
 
   def terminated?
-    @running == false
+    self.running == false
+  end
+
+  def value_exceeds_target?(rule)
+    rule > target_value
   end
 
   def find_broken_chain
     self.target_value = rules[index_end + 1]
-    @running = false
+    self.running = false
     
     rules[index_start..(index_end - 1)].each_with_index do |rule, i|
       rules[(index_start + i + 1)..index_end].each do |rule2|
         if rule + rule2 == target_value
-          @running = true 
+          self.running = true 
           self.index_start += 1
           self.index_end += 1
+        end
+      end
+    end
+  end
+
+  def find_contiguous_set
+    self.running = true
+    rules[0..-2].each_with_index do |rule, i|
+      next if terminated? || value_exceeds_target?(rule)
+      run_inner_loop = true
+      set = [rule]
+      rules[(i + 1)..-1].each do |rule2|
+        next if terminated? || value_exceeds_target?(rule2) || !run_inner_loop
+        set << rule2
+        sum = set.inject(0, :+)
+        if sum == target_value
+          self.running = false
+          self.encryption_weakness = [set.min, set.max].inject(0, :+)
+        elsif sum > target_value
+          run_inner_loop = false
         end
       end
     end
@@ -45,10 +70,13 @@ def load(machine, rules)
   until machine.terminated?
     machine.find_broken_chain
   end
+
+  machine.find_contiguous_set
+
   machine
 end
-
 
 machine = load(VM.new, rules)
 
 puts "Part 1: #{machine.target_value}"
+puts "Part 2: #{machine.encryption_weakness}"
