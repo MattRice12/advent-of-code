a3ce69cc84bf1a95a6c10dcd02ac1e0b7174f4cf
mattrice12 <mattrice12@outlook.com>
2020-12-23 19:39:01 -0600

    part 2 DONE

--- day23/day23.rb
+++ day23/day23.rb
@@ -28,23 +28,84 @@ def part1(data)
   "Part 1: #{cups[1..-1].join("")}"
 end
 
-p part1(data)
+# p part1(data)
+
+def cup_round_2(cups, cups_hash, move, start)
+  cc_key = start
+  cc_value = cups_hash[cc_key]
+
+  frc_key = cups_hash[cc_key]
+  src_key = cups_hash[frc_key]
+  trc_key = cups_hash[src_key]
+  frc_value = cups_hash[frc_key]
+  src_value = cups_hash[src_key]
+  trc_value = cups_hash[trc_key]
+
+  removed_cups = [frc_key, src_key, trc_key]
+
+  n = 1
+  found = false
+  dc_key = cc_key - n
+  until found do
+    raise if n > 4
+    dc_key = cc_key - n <= 0 ? cups.length + 1 - n : cc_key - n
+    if removed_cups.include?(dc_key)
+      n += 1
+    else
+      found = true
+    end
+  end
+
+  # p "cups: (#{cc_key}) #{ordered_cups(cups_hash, cc_key).join(" ")}"
+  # p "pick up: #{removed_cups.join(", ")}"
+  # p "destination: #{dc_key}"
+  # puts
+
+  dc_value = cups_hash[dc_key]
+
+  # current_cup's key => third_removed_cup's value
+  cups_hash[cc_key] = trc_value
+
+
+  # third_removed_cup's key => destination_cup's value
+  cups_hash[trc_key] = dc_value
+
+  
+  # destination_cup's key => current_cups's value
+  cups_hash[dc_key] = cc_value
+
+  cups_hash[cc_key]
+end
 
 def part2(data)
   cups = data[0].split("").map(&:to_i)
   (cups += (10..1_000_000).to_a).flatten
 
-  sorted_cups = cups.sort
+  cups_hash = Hash[cups.zip(cups[1..-1] + [cups[0]])]
+
+  start = cups[0]
   LARGE_MOVES.times do |move|
-    cups = cup_round(cups, sorted_cups)  
+    # p "move: #{move+1}"
+
+    start = cup_round_2(cups, cups_hash, move % cups.length, start)  
   end
 
-  cup_1_index = cups.index(1)
-  p cups[cup_1_index + 1]
-  p cups[cup_1_index + 2]
-  cups_product = cups[cup_1_index + 1] * cups[cup_1_index + 2]
-  "Part 2: #{cups_product}"
+  first = cups_hash[1]
+  second = cups_hash[first]
+  p "first = #{first}"
+  p "second = #{second}"
+  # p "final = #{ordered_cups(cups_hash, 1).join("")}"
+
+  "Part 2: #{first * second}"
 end
 
-# INCOMPLETE
+def ordered_cups(cups_hash, n)
+  final_arr = []
+  while final_arr.length < cups_hash.length - 1
+    final_arr << cups_hash[n]
+    n = cups_hash[n]
+  end
+  final_arr
+end
+
 p part2(data)
