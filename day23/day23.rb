69f8c61a248d57644a60f8502ea4fb192c3471fc
mattrice12 <mattrice12@outlook.com>
2020-12-23 23:39:22 -0600

    refactor

--- day23/day23.rb
+++ day23/day23.rb
@@ -1,25 +1,31 @@
 data = File.open("./data.txt").readlines
 # data = File.open("./data-2.txt").readlines
 
-class Attempt1
+
+# iterating, sorting, inserting, deleting are all at least O(n), which won't scale to 10_000_000 moves
+class InefficientAlgorithm
+  attr_accessor :cups
+
   MOVES = 100
 
-  def self.execute(data)
-    cups = data[0].split("").map(&:to_i)
+  def initialize(data)
+    @cups = data[0].split("").map(&:to_i) 
+  end
 
+  def execute
     sorted_cups = cups.sort
     MOVES.times do |move|
-      cups = cup_round(cups, sorted_cups)  
+      self.cups = play(cups, sorted_cups)  
     end
 
     until cups[0] == 1 do
-      cups << cups.shift
+      self.cups << cups.shift
     end
     p "Part 1: #{cups[1..-1].join("")}"
   end
   
-  def self.cup_round(cups, sorted_cups)
-    cups << cups.shift
+  def play(cups, sorted_cups)
+    self.cups << cups.shift
     removed_cups = [cups.shift, cups.shift, cups.shift]
     new_sorted_cups = sorted_cups - removed_cups
     destination_cup = new_sorted_cups[new_sorted_cups.index(cups[-1]) - 1]
@@ -29,20 +35,31 @@ class Attempt1
   end
 end
 
-Attempt1.execute(data)
+InefficientAlgorithm.new(data).execute
 
-class Attempt2
-  LARGE_MOVES = 10_000_000
 
-  def self.execute(data)
-    cups = data[0].split("").map(&:to_i)
-    (cups += (10..1_000_000).to_a).flatten
+# Using linked list type hashes for O(1) efficiency, which scales to 10_000_000 moves
+class EfficientAlgorithm
+  attr_accessor :cups, :cups_hash
+  
+  MOVES = 10_000_000
+  CUP_MAX = 1_000_000
 
-    cups_hash = Hash[cups.zip(cups[1..-1] + [cups[0]])]
+  def initialize(data)
+    @cups = build_cups(data)
+    @cups_hash = Hash[@cups.zip(@cups[1..-1] + [@cups[0]])]
+  end
 
-    start = cups[0]
-    LARGE_MOVES.times do |move|
-      start = cup_round_2(cups, cups_hash, move % cups.length, start)  
+  def build_cups(data)
+    original_cups = data[0].split("").map(&:to_i)
+    extra_cups = ((original_cups.length + 1)..CUP_MAX).to_a
+    original_cups + extra_cups
+  end
+
+  def execute
+    next_cc_key = cups[0]
+    MOVES.times do |move|
+      next_cc_key = play(move % cups.length, next_cc_key)  
     end
 
     first = cups_hash[1]
@@ -52,54 +69,33 @@ class Attempt2
     p "Part 2: #{first * second}"
   end
 
-
-  def self.cup_round_2(cups, cups_hash, move, start)
-    cc_key = start
-    cc_value = cups_hash[cc_key]
-
+  # cc == current cup
+  # frc == first removed cup
+  # src == second ""
+  # trc == third ""
+  # dc == destination cup
+  def play(move, cc_key)
     frc_key = cups_hash[cc_key]
     src_key = cups_hash[frc_key]
     trc_key = cups_hash[src_key]
-    frc_value = cups_hash[frc_key]
-    src_value = cups_hash[src_key]
-    trc_value = cups_hash[trc_key]
 
     removed_cups = [frc_key, src_key, trc_key]
 
-    dc_key = get_dc_key(cups, removed_cups, cc_key)
+    dc_key = get_dc_key(removed_cups, cc_key)
 
     dc_value = cups_hash[dc_key]
-    cups_hash[cc_key] = trc_value
-    cups_hash[trc_key] = dc_value
-    cups_hash[dc_key] = cc_value
+    self.cups_hash[dc_key] = cups_hash[cc_key]
+    self.cups_hash[cc_key] = cups_hash[trc_key]
+    self.cups_hash[trc_key] = dc_value
 
     cups_hash[cc_key]
   end
 
-  def self.get_dc_key(cups, removed_cups, cc_key)
-    n = 1
-    found = false
-    dc_key = cc_key - n
-    until found do
-      raise if n > 4
-      dc_key = dc_key <= 0 ? cups.length + 1 - n : dc_key
-      if removed_cups.include?(dc_key)
-        n += 1
-      else
-        found = true
-      end
-    end
+  def get_dc_key(removed_cups, cc_key, n = 1)
+    dc_key = cc_key - n < 1 ? cups.length + 1 - n : cc_key - n
+    dc_key = get_dc_key(removed_cups, cc_key, n + 1) if removed_cups.include?(dc_key)
     dc_key
-  end
-
-  def self.ordered_cups(cups_hash, n)
-    final_arr = []
-    while final_arr.length < cups_hash.length - 1
-      final_arr << cups_hash[n]
-      n = cups_hash[n]
-    end
-    final_arr
   end
 end
 
-Attempt2.execute(data)
+EfficientAlgorithm.new(data).execute
