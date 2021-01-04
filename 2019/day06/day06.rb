4ab70254e77fa9b7f230d4ea78ce9e2fceb7acd1
mattrice12 <mattrice12@outlook.com>
2021-01-04 17:51:00 -0600

    day06 2019

---
+++ 2019/day06/day06.rb
@@ -0,0 +1,58 @@
+data = File.open("./data.txt").readlines
+# data = File.open("spec/fixtures/sample_data.txt").readlines
+
+class Orbit
+  attr_accessor :orbits
+
+  def initialize(data)
+    @data = data
+    build_orbit_hash
+  end
+
+  def find_orbit(orbit)
+    body, moon = orbit.split(")")
+    Hash[moon.gsub("\n", ""), body]
+  end
+
+  def build_orbit_hash
+    @orbits ||= {}
+    @data.each do |orbit|
+      @orbits.merge!(find_orbit(orbit))
+    end
+  end
+
+  def count_orbits(moon, total = 0)
+    return total if !@orbits[moon]
+    count_orbits(@orbits[moon], total + 1)
+  end
+
+  def count_all_orbits
+    @orbits
+      .map {|k, v| count_orbits(k)}
+      .sum
+  end
+
+  def track_path(moon, path = [])
+    return path if !@orbits[moon]
+    track_path(@orbits[moon], path + [@orbits[moon]])
+  end
+
+  def intersection(moon_a, moon_b)
+    (track_path(moon_a) & track_path(moon_b))
+  end
+
+  def transfers_between_two_moons(moon_a, moon_b)
+    below_intersection = intersection(moon_a, moon_b)[1..-1]
+    (track_path(moon_a) + track_path(moon_b)).uniq - below_intersection
+  end
+
+  def count_transfers_between(moon_a, moon_b)
+    transfers_between_two_moons(moon_a, moon_b).count - 1
+  end
+end
+
+orbit = Orbit.new(data)
+p "Part 1: #{orbit.count_all_orbits}"
+
+
+p "Part 2: #{orbit.count_transfers_between("YOU", "SAN")}"
---
+++ 2019/day06/spec/orbit_spec.rb
@@ -0,0 +1,74 @@
+require_relative "../day06.rb"
+
+RSpec.describe Orbit do
+  DATA = File.open("spec/fixtures/sample_data.txt").readlines
+  DATA_2 = File.open("spec/fixtures/sample_data_2.txt").readlines
+
+  describe "initialization" do
+    it "initializes" do
+      orbit = Orbit.new([])
+
+      expect(orbit).to be_an_instance_of(Orbit)
+    end
+  end
+
+  describe "build orbit chain" do
+    it "splits string into orbiter and orbitee" do
+      orbit = Orbit.new([])
+
+      expect(orbit.find_orbit("B)C")).to eq({"C" => "B"})
+    end
+    
+    it "build hash of all orbits for each planet" do
+      orbit = Orbit.new(["COM)B", "B)C", "C)D"])
+
+      expect(orbit.orbits).to eq({"B" => "COM", "C" => "B", "D" => "C"})
+    end
+  end
+
+  describe "count orbits" do
+    it "count total orbits for single moon" do
+      orbit = Orbit.new(["COM)B", "B)C", "C)D"])
+
+      expect(orbit.count_orbits("D")).to eq(3)
+    end
+
+    it "count total orbits for all moons" do
+      orbit = Orbit.new(["COM)B", "B)C", "C)D"])
+
+      expect(orbit.count_all_orbits).to eq(6)
+    end
+
+    it "count total orbits in larger data set" do
+      orbit = Orbit.new(DATA)
+
+      expect(orbit.count_all_orbits).to eq(42)
+    end
+  end
+
+  describe "navigating orbits" do
+    it "track path of one orbit" do
+      orbit = Orbit.new(["A)B", "B)C", "C)D", "B)E", "E)F"])  
+
+      expect(orbit.track_path("D")).to eq(["C", "B", "A"])
+    end
+    
+    it "find intersection and below" do
+      orbit = Orbit.new(["A)B", "B)C", "C)D", "B)E", "E)F"])
+
+      expect(orbit.intersection("D", "F")).to eq(["B", "A"])
+    end
+
+    it "find transfers between two moons" do
+      orbit = Orbit.new(["A)B", "B)C", "C)D", "B)E", "E)F"])
+
+      expect(orbit.transfers_between_two_moons("D", "F")).to eq(["C", "B", "E"])
+    end
+
+    it "count transfers between two moons" do
+      orbit = Orbit.new(["A)B", "B)C", "C)D", "B)E", "E)F"])
+
+      expect(orbit.count_transfers_between("D", "F")).to eq(2)
+    end
+  end
+end
